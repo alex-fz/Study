0e68a7d34f47503ec983a192c9c76823f9067668
Zardix <laxis2907v2@gmail.com>
2023-01-18 21:41:20 +0100

    t

--- DataScience/files/ns1.py
+++ DataScience/files/ns1.py
@@ -1,4 +1,14 @@
 import numpy as np
+import pandas as pd
 import mimetypes
 
+
+array = np.linspace(start=100,stop=120,num=50)
+array = array.reshape(10,5)
+
+
+new_arr = np.identity(5)
+
+df = pd.DataFrame(data=new_arr, dtype=int)
+print(df)
 
